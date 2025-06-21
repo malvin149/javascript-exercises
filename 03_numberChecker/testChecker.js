2f7817ada45b9b4bb1134726468aa7f793ce4d41
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-06-21 01:10:47 +0000

    Add solutions and add testChecker.js to debug

---
+++ 03_numberChecker/testChecker.js
@@ -0,0 +1,6 @@
+const numberChecker = require('./numberChecker')
+
+console.log(numberChecker(1000));
+console.log(numberChecker(10));
+console.log(numberChecker(9));
+console.log(numberChecker(6));
