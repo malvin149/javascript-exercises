5ebd0d83e49db5ba70d06e799ecca6319129a600
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-06 20:39:36 +0000

    Finish 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -4,8 +4,15 @@
 // add string to results after every iteration
 // return results 
 // call the function and pass in the arguments.
+const number = () => {
+    Math.floor(Math.random)
+}
+
 
 const repeatString = (string, num) => {
+    if (num < 0) {
+        return 'ERROR';
+    }
     let results = '';
     for (let i = 0; i < num; i++) {
         results += string;
@@ -14,6 +21,12 @@ const repeatString = (string, num) => {
 }
 
 repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
+repeatString('', 10);
+
 
 // Do not edit below this line
 module.exports = repeatString;
