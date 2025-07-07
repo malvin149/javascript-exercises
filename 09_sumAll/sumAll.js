aa302b700e6b885cdd7348e6d2afa6c876dc02b5
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-07 09:39:12 +0000

    Modify 09_sumAll .js and .spec.js
    Check for a valid positive integer
    Determine the start and end points
    Implement the loop and return the sum

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    //check if either number is a positive number or an integer, if false return ERROR
+    if ([num1, num2].some(n => n < 0 || !Number.isInteger(n))) {
+        return 'ERROR';
+    }
+    //Determine the true start and end point for the loop regardless of the order passed
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2);
+    let sum = 0;
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
