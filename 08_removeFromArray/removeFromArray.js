dc70d6b9ec6eeebe7fc76448428e06c4e7e8d45a
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-07 08:30:57 +0000

    Modify removeFromArray .js and .spec.js files

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+// declare a function called removeFromArray 
+// takes two arguments and array and restParams
+// filters the array and returns items that are not included in the restParam
+
+const removeFromArray = function(array, ...elementsToRemove) {
+
+    return array.filter(item => {
+        return !elementsToRemove.includes(item);
+    });
 };
 
 // Do not edit below this line
