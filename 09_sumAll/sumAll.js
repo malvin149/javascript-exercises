a4c9c64339045d4ca7b9dce2f2be026d4b88e1b4
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-07 09:50:16 +0000

    Modify 09_sumAll.js
    Remove the ccurly brakets and move return
    statement to make it a one liner.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,8 +1,6 @@
 const sumAll = function(num1, num2) {
     //check if either number is a positive number or an integer, if false return ERROR
-    if ([num1, num2].some(n => n < 0 || !Number.isInteger(n))) {
-        return 'ERROR';
-    }
+    if ([num1, num2].some(n => n < 0 || !Number.isInteger(n))) return 'ERROR';
     //Determine the true start and end point for the loop regardless of the order passed
     const start = Math.min(num1, num2);
     const end = Math.max(num1, num2);
