64a9fe861da132c7223c10a71ae6df4b99e9df45
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-12 12:18:04 +0000

    Implement temperature conversion function to pass tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  //formula = (F - 32) / 1.8
+  let converted = ((temperature - 32) / 1.8).toFixed(1);
+  return parseFloat(converted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  // formula = C * 1.8 + 32
+  let converted = ((temperature * 1.8) + 32).toFixed(1);
+  return parseFloat(converted);
 };
 
 // Do not edit below this line
