3f591617f52a7de6edf08969cde2855ed35c6d37
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-12 22:06:03 +0000

    Resolve previous failing test
    Addresses previously failing test by providing the
    'add', 'subtract', and 'sum' functions.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,13 +1,20 @@
-const add = function() {
-	
+const add = function(a, b) {
+	const value = a + b;
+  return value;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	const value = a - b;
+  return value;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (!Array.isArray(arr) || arr.length === 0) {
+    return 0;
+  } 
+  const total = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue}, 0);
+  return total;
 };
 
 const multiply = function() {
