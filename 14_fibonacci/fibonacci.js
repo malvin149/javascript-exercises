a85afea687e24652e6d66a99af90877c292361e4
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-13 22:17:20 +0000

    Feat(fibonacci): Implement Nth number with input validation
    Handles string inputs, negative numbers,
    non-numeric strings by returning 'OOPS'.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function( num ) {
+    const number = Number( num );
+    
+    if ( number < 0 || !Number.isInteger(number) ) return 'OOPS';
+    if (number === 0 || number === 1) return number;
+    
+    let firstNum = 0;
+    let secondNum = 1;
 
+    for (let i = 2; i <= number; i++) {
+        let fibonacciNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = fibonacciNum;
+        
+    }
+     return secondNum
 };
 
 // Do not edit below this line
