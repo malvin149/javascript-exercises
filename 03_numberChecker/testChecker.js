a4d79c1b092ae3ae5c6a50867050ad90ab61c5e5
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-13 09:42:05 +0000

    Delete 03_numberChecker/testChecker.js, added to test function

--- 03_numberChecker/testChecker.js
+++
@@ -1,6 +0,0 @@
-const numberChecker = require('./numberChecker')
-
-console.log(numberChecker(1000));
-console.log(numberChecker(10));
-console.log(numberChecker(9));
-console.log(numberChecker(6));
