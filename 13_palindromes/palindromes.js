badbc5da6b8b9ed9ef98d5cb5c339a8babd880e5
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-13 11:33:52 +0000

    Fix function to pass test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase()
+    // replace any characters ![a-zA-Z0-9] 
+    .replace(/[^a-zA-Z0-9]/g, '');
 
+    const reversedString = cleanString.split('').reverse().join('');
+
+    return reversedString === cleanString;
 };
 
 // Do not edit below this line
