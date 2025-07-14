684669aea1d18fca33bd0a81acea5a68ae096a6a
malvin149 <205408852+malvin149@users.noreply.github.com>
2025-07-14 21:13:41 +0000

    Feat(findTheOldest): Implement find the oldest Function
    - Added a helper function to get the age by taking
    in the birth and death year as params.
    - Validate length of object array or argument
    parsed is an object.
    - Return the oldest person by comparing age with
    the next person in the array.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const getAge = ( birthYear, deathYear) => {
+    if (!deathYear) return new Date().getFullYear() - birthYear; 
+    return deathYear - birthYear;
+}
 
+const findTheOldest = function( people ) {
+    if ((!Array.isArray(people)) || (people.length === 0)) return undefined;
+
+    return people.reduce(( oldest, next ) => {
+        const ageOfOldest = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+
+        const ageOfCurrent = getAge(next.yearOfBirth, next.yearOfDeath);
+
+        return (ageOfCurrent > ageOfOldest) ? next : oldest;
+    })
 };
 
 // Do not edit below this line
