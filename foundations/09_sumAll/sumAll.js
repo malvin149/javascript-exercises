0ec56be2e2d669869cdcbe7491c42eed0868939f
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-02 22:53:29 +0000

    Complete 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let totalSum = 0
 
+    if (typeof start !== 'number' || typeof end !== 'number') return 'ERROR';
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+    if (start < 0 || end < 0) return 'ERROR';
+    if (start > end) {
+        let temp = start; 
+            start = end;
+            end = temp;
+    }
+    for (let i = start; i <= end; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
