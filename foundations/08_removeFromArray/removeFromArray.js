bed00eb62ce96f819a9a0240c77328afe3984e08
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-02 20:53:34 +0000

    Complete 08_removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    for (let i = 0; i < items.length; i++) {
+        let index = array.indexOf(items[i]);
+        while (index !== -1) {
+            array.splice(index, 1)
+            index = array.indexOf(items[i]);
+        };
+    }
+    return array;
 };
 
 // Do not edit below this line
