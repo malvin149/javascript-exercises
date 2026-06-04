8ab290b3451c6a56d94c663917dd310bacf7e757
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-04 15:21:34 +0000

    Complete 12_calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	let total = number1 + number2;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	let difference = number1 - number2;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let totalSum = 0;
+  for (let i = 0; i < array.length; i++) {
+    totalSum += array[i];
+  }
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let totalProduct = 1;
+  for (let i = 0; i < array.length; i++) {
+    totalProduct *= array[i];
+  }
+  return totalProduct;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let result = base ** exponent;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+
+  let totalProduct = 1;
+  for (let i = number; i >= 1; i--) {
+    totalProduct *= i;
+  }
+  return totalProduct;
 };
 
 // Do not edit below this line
