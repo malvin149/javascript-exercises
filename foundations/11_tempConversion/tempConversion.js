2f9cb1496fe3a999d5b9470f060aad0fc42a0928
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-04 12:00:01 +0000

    Complete 11_tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const celsius = ( temperature - 32 ) * ( 5 / 9 );
+  let results = Number(celsius.toFixed(1));
+  return results;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const fahrenheit = ( temperature * ( 9 / 5 )) + 32;
+  let results = Number(fahrenheit.toFixed(1));
+  return results;
 };
 
 // Do not edit below this line
