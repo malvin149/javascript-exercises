824af57fb5c59ad44866ed73e0d23f005df4c4bc
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-04 17:42:10 +0000

    Complete 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes =function (string) {
+    let cleanedString = string.toLowerCase().replace(/[^\w]/g, '');
+    let reversedString = cleanedString.split('').reverse().join('');
 
+    if (cleanedString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
