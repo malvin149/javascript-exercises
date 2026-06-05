7c31440377ead8eedca1ce993898d6ad789e429c
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-05 12:10:17 +0000

    Complete 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    position = Number(position);
+    if (position < 0) return 'OOPS';
+    if (position === 0) return 0;
+    if (position === 1 || position === 2) return 1;
 
+    let prev = 1;
+    let curr = 1;
+
+    for (let i = 3; i <= position; i++) {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 };
 
 // Do not edit below this line
