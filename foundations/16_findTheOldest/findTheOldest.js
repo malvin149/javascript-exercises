8759a19973f352e944e5655088dad47acf023040
malvin149 <205408852+malvin149@users.noreply.github.com>
2026-06-20 23:31:42 +0000

    Complete findTheOldest exercise using getAge helper and reduce

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const getAge = (person) => 
+    person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth;
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => 
+        getAge(oldest) > getAge(person) ? oldest : person
+    , people[0])
 };
 
 // Do not edit below this line
